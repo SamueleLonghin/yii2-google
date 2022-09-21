59912587465a7c8dba9db5b037a2a2a8bde8705d
Samuele Longhin <longhin.samuele@outlook.com>
2022-09-21 12:03:26 +0200

    removing scope

--- src/GoogleAuthTrait.php
+++ src/GoogleAuthTrait.php
@@ -3,25 +3,48 @@
 namespace samuelelonghin\google\auth;
 
 use Google\Client;
+use Google\Exception;
 use Yii;
 
-trait  GoogleAuthTrait
+trait GoogleAuthTrait
 {
+    private ?Client $_client = null;
 
     private function checkServiceAccountCredentialsFile()
     {
         // service account creds
         $application_creds = Yii::$app->params['google-service-account-credentials'];
-
         return file_exists($application_creds) ? $application_creds : false;
     }
 
-    private function missingServiceAccountDetailsWarning()
+    /**
+     * @throws Exception|Exception
+     */
+    public function getClient(): ?Client
     {
-        $ret = "
+        if (!$this->_client) {
+            $this->_client = new Client();
+
+            if ($credentials_file = $this->checkServiceAccountCredentialsFile()) {
+                // set the location manually
+                $this->_client->setAuthConfig($credentials_file);
+            } elseif (getenv('GOOGLE_APPLICATION_CREDENTIALS')) {
+                // use the application default credentials
+                $this->_client->useApplicationDefaultCredentials();
+            } else {
+                echo $this->missingServiceAccountDetailsWarning();
+                return null;
+            }
+        }
+        return $this->_client;
+    }
+
+    private function missingServiceAccountDetailsWarning(): string
+    {
+        return "
     <h3 class='warn'>
       Warning: You need download your Service Account Credentials JSON from the
-      <a href='http://developers.google.com/console'>Google API console</a>.
+      <a href='https://developers.google.com/console'>Google API console</a>.
     </h3>
     <p>
       Once downloaded, move them into the root directory of this repository and
@@ -31,29 +54,5 @@ trait  GoogleAuthTrait
       In your application, you should set the GOOGLE_APPLICATION_CREDENTIALS environment variable
       as the path to this file, but in the context of this example we will do this for you.
     </p>";
-
-        return $ret;
-    }
-
-     /**
-     * @throws Exception
-     */
-    public function getClient(): ?Client
-    {
-        $this->_client = new Client();
-
-        if ($credentials_file = $this->checkServiceAccountCredentialsFile()) {
-            // set the location manually
-            $this->_client->setAuthConfig($credentials_file);
-        } elseif (getenv('GOOGLE_APPLICATION_CREDENTIALS')) {
-            // use the application default credentials
-            $this->_client->useApplicationDefaultCredentials();
-        } else {
-            echo $this->missingServiceAccountDetailsWarning();
-            return null;
-        }
-
-        $this->_client->addScope("https://www.googleapis.com/auth/drive");
-        return $this->_client;
     }
 }
